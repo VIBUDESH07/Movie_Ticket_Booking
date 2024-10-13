57881cdbf669545b5f233d0b43421f54499900e4
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-10-13 20:06:57 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,24 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const movieRoutes = require('./Routes/MovieRoutes');
+const dotenv = require('dotenv');
+const authRoutes = require('./Routes/auth');
+const { authenticateToken } = require('./Middleware/authmiddleware');
 
+dotenv.config();
 const app = express();
-
-// Middleware
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/movieDB', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
-  .catch((error) => console.error('MongoDB connection error:', error));
-
-// Routes
-app.use('/movies', movieRoutes);
-
-// Start the server
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+  .catch((err) => console.error(err));
+app.use('/api/auth', authRoutes);
+app.use(authenticateToken)
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
