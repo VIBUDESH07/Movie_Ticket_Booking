56c31e0c11dc331188187d69e127c6b796477c4e
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-10-23 20:09:33 +0530

    Update App.jsx

--- Movie/src/App.jsx
+++ Movie/src/App.jsx
@@ -1,15 +1,19 @@
 
-import './App.css'
-import AddMovie from './Components/AddMovie'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+/* import Login from './Components/Login/Login'; */
+import SignUp from './Components/Login/SignUp';
+import { AddTheatre } from './Components/Theatres/AddTheatre';
 
-function App() {
- 
-
+const App = () => {
   return (
-    <>
-    <AddMovie/>
-         </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<AddTheatre/>} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<h1>Welcome to the Dashboard!</h1>} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
